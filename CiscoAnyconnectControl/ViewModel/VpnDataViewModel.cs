d6f0a5f9a7ff5c269b60b189954da9ce7bec74b1
JannesP <jannespeters.privat@gmail.com>
2017-10-22 17:12:02 +0200

    Save stuff because computer is unstable.

---
+++ CiscoAnyconnectControl/ViewModel/VpnDataViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CiscoAnyconnectControl.ViewModel
+{
+    class VpnDataViewModel
+    {
+        public string Address => "vpn.example.com";
+        public string Username => "username";
+        public string Password => "password";
+
+    }
+}
---
+++ CiscoAnyconnectControl/ViewModel/VpnStatusViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CiscoAnyconnectControl.ViewModel
+{
+    class VpnStatusViewModel
+    {
+        public string Color => "#FFBF0000";
+        public string Text => "Disconnected ...";
+    }
+}
